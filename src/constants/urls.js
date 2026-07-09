39647edfc522bb9bce3e4c8b956a2fd3fe0456e1
Rehan Ghani <rehanghani366@gmail.com>
2026-07-10 00:23:35 +0530

    feat: add central constants file for API endpoints and URLs

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -3,7 +3,7 @@ export const LOCAL_URL = "http://localhost:5000";
 export const PROD_URL = "https://jamia-madarsha-server.onrender.com";
 
 // Auto detect environment 
-export const BACKEND_URL = LOCAL_URL;
+export const BACKEND_URL = PROD_URL;
 
 export const BASE_URL = `${BACKEND_URL}/api`;
 
